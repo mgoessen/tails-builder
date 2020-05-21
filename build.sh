d7f0f80e99b6fc4d5fe6a0c2a8f3f2f372e84214
geb <gebura@poolp.org>
2020-05-21 07:34:36 +0200

    try to debug vagrant ssh

--- build.sh
+++ build.sh
@@ -15,4 +15,9 @@ chmod g+rx $HOME
 #chmod 755 -R $HOME/tails
 #chmod 755 -R $HOME/tails/vagrant
 
+cd vagrant
+sudo su travis -c 'vagrant up --provider=libvirt'
+sudo su travis -c 'vagrant ssh -c "echo \'hello world!\"'
+
+
 sudo su travis -c "rake build" && sudo su travis -c "rake vm:halt"
