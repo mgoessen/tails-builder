3371615f9cb6adefe17f2c1132da5f5bb66eea79
geb <gebura@poolp.org>
2020-05-21 06:43:46 +0200

    lets check permissions; removed a bit of debug

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-sudo ruby -v
-sudo gem -v
-sudo gem list
-sudo gem list --local
-sudo gem environment
-
+gem environment
 
 git clone https://git-tails.immerda.ch/tails && \
 cd tails && \
 git checkout devel && \
 git submodule update --init
+
+ls -alh $HOME
+ls -alh tails
+ls -alh tails/vagrant
+chmod 755 -R $HOME
+chmod 755 -R $HOME/tails
+chmod 755 -R $HOME/tails/vagrant
 
 sudo su travis -c "rake build" && sudo su travis -c "rake vm:halt"
