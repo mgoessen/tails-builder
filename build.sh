08dac5835a417cd1b596bed0a40c39cb2becfbc6
geb <gebura@poolp.org>
2020-05-22 00:18:42 +0200

    remove ssh debug, add commented to for vm:destroy

--- build.sh
+++ build.sh
@@ -7,17 +7,9 @@ cd tails && \
 git checkout devel && \
 git submodule update --init
 
-ls -alh $HOME
-chmod g+rx $HOME
-
-cd vagrant
-sudo su travis -c 'vagrant up --provider=libvirt'
-
-ls -alh /home/travis/.ssh
-ls -alh /home/travis/.ssh/config
+chmod 755 $HOME
 chmod 755 /home/travis/.ssh
 chmod 755 /home/travis/.ssh/config
 
-sudo su travis -c 'vagrant ssh -c "echo test"'
-
+#sudo su travis -c "rake vm:provision" && sudo su travis -c "rake vm:destroy"
 sudo su travis -c "rake build" && sudo su travis -c "rake vm:halt"
