4601ae71fa6d32b545aa847b2d41d802b9dc985a
geb <gebura@poolp.org>
2020-05-21 04:45:45 +0200

    added debug, added sudo for build (yeah..)

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+sudo ruby -v
+sudo gem -v
+sudo gem list
+sudo gem list --local
+sudo gem environment
+
+
 git clone https://git-tails.immerda.ch/tails && \
 cd tails && \
 git checkout devel && \
 git submodule update --init
 
-rake build --trace && rake vm:halt
+sudo rake build --trace && rake vm:halt
