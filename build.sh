d636d5e34ed3897950752d928c842418466d04f1
geb <gebura@poolp.org>
2020-05-22 08:55:49 +0200

    Revert "lets try to build 4.6"
    This reverts commit 18961306dac1d67650cb31bbf6375bf8120d3cbb.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ gem environment
 
 git clone https://git-tails.immerda.ch/tails && \
 cd tails && \
-git checkout -b 4.6 4.6 && \
+git checkout stable && \
 git submodule update --init
 
 chmod 755 $HOME
